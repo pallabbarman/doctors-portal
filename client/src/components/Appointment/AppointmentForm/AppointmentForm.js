d773455f88d20d2b00827ee2a8d1a8fae9dc0d0e
Pallab Barman <76774117+pallabbarman@users.noreply.github.com>
2021-04-11 12:29:26 +0600

    modal added

---
+++ client/src/components/Appointment/AppointmentForm/AppointmentForm.js
@@ -0,0 +1,126 @@
+/* eslint-disable react/jsx-props-no-spreading */
+import React from 'react';
+import { useForm } from 'react-hook-form';
+import Modal from 'react-modal';
+
+const customStyles = {
+    content: {
+        top: '50%',
+        left: '50%',
+        right: 'auto',
+        bottom: 'auto',
+        marginRight: '-50%',
+        transform: 'translate(-50%, -50%)',
+    },
+};
+
+Modal.setAppElement('#root');
+
+const AppointmentForm = (props) => {
+    const { date, modalIsOpen, appointmentOn, closeModal } = props;
+    const {
+        register,
+        handleSubmit,
+        formState: { errors },
+    } = useForm();
+    const onSubmit = (data) => {
+        console.log(data);
+        closeModal();
+    };
+    return (
+        <div>
+            <Modal
+                isOpen={modalIsOpen}
+                onRequestClose={closeModal}
+                style={customStyles}
+                contentLabel="Example Modal"
+            >
+                <h2 className="text-brand text-center">{appointmentOn}</h2>
+                <p className="text-secondary text-center">
+                    <small>ON {date.toDateString()}</small>
+                </p>
+                <form className="p-5" onSubmit={handleSubmit(onSubmit)}>
+                    <div className="form-group">
+                        <input
+                            type="text"
+                            {...register('name', { required: true })}
+                            placeholder="Your Name"
+                            className="form-control"
+                        />
+                        {errors.name && <span className="text-danger">This field is required</span>}
+                    </div>
+                    <div className="form-group">
+                        <input
+                            type="text"
+                            {...register('phone', { required: true })}
+                            placeholder="Phone Number"
+                            className="form-control"
+                        />
+                        {errors.phone && (
+                            <span className="text-danger">This field is required</span>
+                        )}
+                    </div>
+                    <div className="form-group">
+                        <input
+                            type="text"
+                            {...register('email', { required: true })}
+                            placeholder="Email"
+                            className="form-control"
+                        />
+                        {errors.email && (
+                            <span className="text-danger">This field is required</span>
+                        )}
+                    </div>
+                    <div className="form-group row">
+                        <div className="col-4">
+                            <select
+                                className="form-control"
+                                {...register('gender', { required: true })}
+                            >
+                                <option disabled value="Not set">
+                                    Select Gender
+                                </option>
+                                <option value="Male">Male</option>
+                                <option value="Female">Female</option>
+                                <option value="Not set">Other</option>
+                            </select>
+                            {errors.gender && (
+                                <span className="text-danger">This field is required</span>
+                            )}
+                        </div>
+                        <div className="col-4">
+                            <input
+                                {...register('age', { required: true })}
+                                className="form-control"
+                                placeholder="Your Age"
+                                type="number"
+                            />
+                            {errors.age && (
+                                <span className="text-danger">This field is required</span>
+                            )}
+                        </div>
+                        <div className="col-4">
+                            <input
+                                {...register('weight', { required: true })}
+                                className="form-control"
+                                placeholder="Weight"
+                                type="number"
+                            />
+                            {errors.weight && (
+                                <span className="text-danger">This field is required</span>
+                            )}
+                        </div>
+                    </div>
+
+                    <div className="form-group text-right">
+                        <button type="submit" className="btn btn-brand">
+                            Send
+                        </button>
+                    </div>
+                </form>
+            </Modal>
+        </div>
+    );
+};
+
+export default AppointmentForm;
--- client/src/components/Appointment/BookingCard/BookingCard.js
+++ client/src/components/Appointment/BookingCard/BookingCard.js
@@ -1,8 +1,17 @@
-import React from 'react';
+import React, { useState } from 'react';
+import AppointmentForm from '../AppointmentForm/AppointmentForm';
 
 const BookingCard = (props) => {
-    const { booking } = props;
+    const { booking, date } = props;
     const { subject, visitingHour, totalSpace } = booking;
+    const [modalIsOpen, setIsOpen] = useState(false);
+    function openModal() {
+        setIsOpen(true);
+    }
+
+    function closeModal() {
+        setIsOpen(false);
+    }
     return (
         <div className="col-md-4 mb-5">
             <div className="card p-3">
@@ -10,9 +19,19 @@ const BookingCard = (props) => {
                     <h5 className="card-title text-brand">{subject}</h5>
                     <h6>{visitingHour}</h6>
                     <p>{totalSpace} SPACES AVAILABLE</p>
-                    <button type="button" className="btn btn-brand text-uppercase">
+                    <button
+                        onClick={openModal}
+                        type="button"
+                        className="btn btn-brand text-uppercase"
+                    >
                         Book Appointment
                     </button>
+                    <AppointmentForm
+                        appointmentOn={subject}
+                        modalIsOpen={modalIsOpen}
+                        closeModal={closeModal}
+                        date={date}
+                    />
                 </div>
             </div>
         </div>
