f61586dde20d100df54dfc46b4ee160eb0619f01
Pallab Barman <76774117+pallabbarman@users.noreply.github.com>
2021-04-12 17:00:10 +0600

    doctors added

--- server/index.js
+++ server/index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const fileUpload = require('express-fileupload');
 const cors = require('cors');
 const { MongoClient } = require('mongodb');
 require('dotenv').config();
@@ -8,12 +9,15 @@ const port = 5000;
 
 app.use(cors());
 app.use(express.json());
+app.use(express.static('doctors'));
+app.use(fileUpload());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.nfge0.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
 
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 client.connect(() => {
     const appointmentCollection = client.db('doctorsPortal').collection('appointments');
+    const doctorCollection = client.db('doctorsPortal').collection('doctors');
 
     app.post('/addAppointment', (req, res) => {
         const appointment = req.body;
@@ -31,6 +35,28 @@ client.connect(() => {
     app.post('/appointmentsByDate', (req, res) => {
         const date = req.body;
         appointmentCollection.find({ date: date.date }).toArray((err, documents) => {
+            res.send(documents);
+        });
+    });
+
+    app.post('/addADoctor', (req, res) => {
+        const { file } = req.files;
+        const { name } = req.body;
+        const { email } = req.body;
+        console.log(name, email, file);
+        file.mv(`${__dirname}/doctors/${file.name}`, (err) => {
+            if (err) {
+                console.log(err);
+                return res.status(500).send({ msg: 'Failed to upload Image' });
+            }
+            doctorCollection.insertOne({ name, email, img: file.name }).then((result) => {
+                res.send(result.insertedCount > 0);
+            });
+        });
+    });
+
+    app.get('/doctors', (req, res) => {
+        doctorCollection.find({}).toArray((err, documents) => {
             res.send(documents);
         });
     });
